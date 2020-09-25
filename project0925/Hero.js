6bd39371309b78780ce0081f66a27d79d4eb4ff4
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-09-25 17:41:30 +0900

    c

--- project0925/Hero.js
+++ project0925/Hero.js
@@ -6,6 +6,14 @@ class Hero{
     //아래의 생성자함수를호출하는 자는, 반드시 주인공 이미지가 붙게될 
     //부모 요소를전달해야 한다..즉 container 변수값을 결정지어야 한다..
     constructor(container,x,y,width,height){
+        //constructor안에 선언된 변수는 현재 인스턴스꺼다!!
+        //this가 붙어있는 변수는, 해당 인스턴스와 생명력을 같이한다.
+        //즉 해당 인스턴스마다 각각 고유하게 보유할 수 있는 변수 
+        //언제 소멸되나? 인스턴스가 살아있는 한...
+        this.babo="바봉";
+        this.x=x;
+        this.y=y;
+
         //생성자내에서는 반드시 변수명앞에 this를 명시해야한다..
         //왜? 객체안에서의 코드작성이므로..
         this.img=document.createElement("img");// <img>

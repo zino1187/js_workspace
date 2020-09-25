9aa7ba9bda64be836bf4b38d46ed78537d3338d6
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-09-25 16:48:28 +0900

    c

--- project0925/Enemy.js
+++ project0925/Enemy.js
@@ -1,9 +1,9 @@
 /*적군을 정의한다!!*/
 class Enemy{
     //적군이 태어날때 어떤 속성(특징)으로 태어날지를 결정하는 생성자 
-    constructor(container, width,height, x, y){
+    constructor(container, width,height, x, y,src){
         this.img=document.createElement("img");
-        this.img.src="../images/shooting/e1.png";
+        this.img.src=src;
         this.img.style.width=width+"px";
         this.img.style.height=height+"px";
         this.img.style.position="absolute";

1aba9fc2ad411817a93ab7c910039af79a7abf87
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-09-25 17:15:16 +0900

    c

--- project0925/Bullet.js
+++ project0925/Bullet.js
@@ -1,8 +1,16 @@
 /*총알을 정의한다*/
 class Bullet{
-    constructor(){
-        
+    constructor(container, x, y){
+        this.img=document.createElement("img");
+        this.img.src="../images/shooting/ball.png";
+        this.img.style.width=30+"px";
+        this.img.style.height=30+"px";
+        this.img.style.position="absolute";
+        this.img.style.left=x+"px";//주인공의 좌표와 관련
+        this.img.style.top=y+"px";//주인공의 좌표와 관련
+        container.appendChild(this.img);
     }
+    
     move(){
 
     }

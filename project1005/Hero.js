a762c28b68808daeff633ee5f443e7afeaa29a38
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-10-05 14:06:23 +0900

    c

--- project1005/Hero.js
+++ project1005/Hero.js
@@ -5,7 +5,7 @@ class Hero{
     //모든 인스턴스가 같은 클래스(거푸집)로 부터 생성되다 보니, 모두 동일한모습으로 태어난다.
     //따라서 각각의 인스턴스마다 개성있는 데이터로 초기화 되려면, 
     //초기화를 시킬수 있는 영역이 필요하며, 이 영역이 바로 생성자 메서드 영역이다..
-    constructor(x,y,width,height){
+    constructor(x,y,width,height,velX, velY){
         console.log("저 지금 태어나면서 , 초기화 됩니다~");
 
         //this가 붙은 변수는, 해당 인스턴스마다 보유하게되며, 인스턴스가 소멸할때까지 생명을
@@ -14,6 +14,8 @@ class Hero{
         this.y=y;
         this.width=width;
         this.height=height;
+        this.velX=velX; //x축으로의 속도
+        this.velY=velY; //y축으로의 속도
 
         //태어날때, 이 객체의 속성을 작성하면 됨...
         this.img=document.createElement("img");
@@ -32,6 +34,7 @@ class Hero{
 
     //주인공의 물리량 변화 
     tick(){
+        //속도는, 키보드에 의해 제어하자!!
         this.x+=5;
         this.y+=5;
     }

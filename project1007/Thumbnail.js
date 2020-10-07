88821805dbcc368cddabe2ba35b02b80aff80f1d
DESKTOP-R2NKGM0\admin <zino1187@gmail.com>
2020-10-07 11:13:46 +0900

    cc

--- project1007/Thumbnail.js
+++ project1007/Thumbnail.js
@@ -1,6 +1,6 @@
 /*영화 이미지 썸네일을 정의한다!!*/
 class Thumbnail{
-    constructor(container, width, height, bd, src){
+    constructor(container, width, height, bd, src, x){
         this.container=container; //어디에 넣을지 컨테이너를 동적으로 받자
         this.div=document.createElement("div");
         this.width=width;
@@ -8,16 +8,18 @@ class Thumbnail{
         this.bd=bd; //보더 두께 
         this.img=document.createElement("img");
         this.src=src;
+        this.x=x; //div의 x좌표
         
         //스타일 부여 
+        this.div.style.position="absolute";
+        this.div.style.left=this.x+"px";
         this.div.style.width=this.width+"px";
         this.div.style.height=this.height+"px";
         this.div.style.border=this.bd+"px solid blue";
         this.div.style.boxSizing="border-box";
-        this.img.style.boxSizing="border-box";
         this.img.src=this.src; //이미지 대입
-        this.img.style.width=this.width+"px";
-        this.img.style.height=this.height+"px";
+        this.img.style.width=(this.width-(bd*2))+"px";
+        this.img.style.height=(this.height-(bd*2))+"px";
 
         //조립 
         this.div.appendChild(this.img);//div에 이미지 넣기 
